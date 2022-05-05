bf3634ad93a349f9491fec88913db94d53772827
Pannu786 <thepannu786@gmail.com>
2022-05-05 15:59:49 +0530

    setup container folder with all the cmp's

--- src/App.js
+++ src/App.js
@@ -1,6 +1,17 @@
+import { About, Footer, Header, Skills, Testimonial, Work } from './container';
 
 const App = () => {
-  return <div>App</div>;
+  return (
+    <div className='app'>
+      <Header />
+      <About />
+      <Footer />
+      <Work />
+      <Skills />
+      {/* <Testimonial /> //* keeping this out for now as I don't have a testimonial yet */}
+      <Footer />
+    </div>
+  );
 };
 
 export default App;
